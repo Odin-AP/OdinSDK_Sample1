5ede77850a5749152e2af79a63a258e658dd4461
Odin-AP <odin.song@adpopcorn.com>
2025-01-01 21:01:15 +0900

    [add] OdinSDK_Sample1 추가

---
+++ OdinSDK_Sample1.xcframework/ios-arm64_x86_64-simulator/OdinSDK_Sample1.framework/Headers/OdinSDK_Sample1.h
@@ -0,0 +1,18 @@
+//
+//  OdinSDK_Sample1.h
+//  OdinSDK_Sample1
+//
+//  Created by Odin.송황호 on 1/1/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OdinSDK_Sample1.
+FOUNDATION_EXPORT double OdinSDK_Sample1VersionNumber;
+
+//! Project version string for OdinSDK_Sample1.
+FOUNDATION_EXPORT const unsigned char OdinSDK_Sample1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OdinSDK_Sample1/PublicHeader.h>
+
+
